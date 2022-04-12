646652bdf8ee1411fad28e3d52519cd8f3a8425c
Nagaraj <nagaraj.cs18@bitsathy.ac.in>
2022-04-12 23:50:17 +0530

    added relationship between producer and movies

--- Models/Producer.cs
+++ Models/Producer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace eTickets.Models
 {
@@ -10,5 +11,7 @@ namespace eTickets.Models
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+        //relationships
+        public List<Movie> Movies { get; set; }
     }
 }
